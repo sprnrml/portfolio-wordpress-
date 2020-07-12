b390d77dfb3716cfa12b9c385757ced13d448ec8
kazuma kobari <sprnrml_crr@icloud.com>
2020-07-13 00:38:44 +0900

    news edit

--- index.php
+++ index.php
@@ -23,7 +23,6 @@
         </ul>
         <ul>
           <li class="dropdown">詳しく
-
             <ul class="dropdown_menu">
               <li><?php $args = array('menu' => '趣味', 'menu_class' => 'menu', 'container' => false,);
                   wp_nav_menu($args); ?></li>
@@ -40,7 +39,6 @@
         </ul>
         <ul>
           <li class="dropdown">趣味
-
             <ul class="dropdown_menu">
               <li><?php $args = array('menu' => '趣味', 'menu_class' => 'menu', 'container' => false,);
                   wp_nav_menu($args); ?></li>
@@ -52,15 +50,16 @@
     <main>
       <div class="main_slider">
         <ul class="slider">
-          <img src="<?php bloginfo('template_directory'); ?>/img/slide-3.jpg" alt="image">
-          <img src="<?php bloginfo('template_directory'); ?>/img/slide-5.jpg" alt="image">
-          <img src="<?php bloginfo('template_directory'); ?>/img/slide-6.jpg" alt="image4">
-          <img src="<?php bloginfo('template_directory'); ?>/img/slide-7.jpg" alt="image4">
-          <img src="<?php bloginfo('template_directory'); ?>/img/slide-8.jpg" alt="image4">
+          <img src="<?php bloginfo('template_directory'); ?>/img/slide-3.jpg" alt="image3">
+          <img src="<?php bloginfo('template_directory'); ?>/img/slide-5.jpg" alt="image5">
+          <img src="<?php bloginfo('template_directory'); ?>/img/slide-6.jpg" alt="image6">
+          <img src="<?php bloginfo('template_directory'); ?>/img/slide-7.jpg" alt="image7">
+          <img src="<?php bloginfo('template_directory'); ?>/img/slide-8.jpg" alt="image8">
         </ul>
       </div>
     </main>
-    <div class="news">新着記事
+    <div class="news">
+      <div class="newe_title">新着情報</div>
      <?php $wpwp_posts = new WP_Query(array('posts_per_page' => 3));
       ?>
       <?php if ($wpwp_posts->have_posts()) :
@@ -73,14 +72,13 @@
                 <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
               </dt>
             </dl>
-
           </div>
         <?php endwhile; ?>
         <?php wp_reset_postdata(); ?>
       <?php else : ?>
         記事はありません。
       <?php endif; ?>
-      <div class="news_achive">
+      <div class="news_archive">
         <a href="<?php echo get_post_type_archive_link('example'); ?>">＜＜記事一覧＞＞</a>
       </div>
     </div>
