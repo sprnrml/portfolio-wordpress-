0f237c7f4cdf0cd4588bd40ebf43b173483aed54
kazuma kobari <sprnrml_crr@icloud.com>
2020-07-13 22:34:53 +0900

    2020/7/13

--- functions.php
+++ functions.php
@@ -32,3 +32,60 @@ wp_enqueue_style('fontawesome','https://use.fontawesome.com/releases/v5.2.0/css/
 
 }
 add_action( 'wp_enqueue_scripts', 'themes_file_scripts' );
+
+/*カスタムメニュー
+---------------------------------------------------------- */
+add_theme_support( 'menus' );
+
+/*固定ページスラッグ表示
+---------------------------------------------------------- */
+function add_page_columns_name($columns) {
+  $columns['slug'] = "スラッグ";
+  return $columns;
+}
+function add_page_column($column_name, $post_id) {
+  if( $column_name == 'slug' ) {
+    $post = get_post($post_id);
+    $slug = $post->post_name;
+    echo attribute_escape($slug);
+  }
+}
+add_filter( 'manage_pages_columns', 'add_page_columns_name');
+add_action( 'manage_pages_custom_column', 'add_page_column', 10, 2);
+
+/*投稿、固定ページ画像表示（url）
+---------------------------------------------------------- */
+function imagepassshort($arg) {
+  $content = str_replace('"img/', '"' . get_bloginfo('template_directory') . '/img/', $arg);
+  return $content;
+}
+add_action('the_content', 'imagepassshort');
+
+/*固定ページデフォルトの＜ｐ＞、＜ｂｒ＞削除
+---------------------------------------------------------- */
+add_filter('the_content', 'wpautop_filter', 9);
+function wpautop_filter($content) {
+global $post;
+$remove_filter = false;
+$arr_types = array('page'); //適用させる投稿タイプを指定
+$post_type = get_post_type( $post->ID );
+if (in_array($post_type, $arr_types)) $remove_filter = true;
+if ( $remove_filter ) {
+remove_filter('the_content', 'wpautop');
+remove_filter('the_excerpt', 'wpautop');
+}
+return $content;
+}
+
+add_action( 'init', 'my_add_pages_categories' ) ;
+function my_add_pages_categories()
+{
+    register_taxonomy_for_object_type( 'category', 'page' ) ;
+}
+add_action( 'pre_get_posts', 'my_set_page_categories' ) ;
+function my_set_page_categories( $query )
+{
+    if ( $query->is_category== true && $query->is_main_query()){
+        $query->set( 'post_type', array( 'post', 'page', 'nav_menu_item' )) ;
+    }
+}
--- index.php
+++ index.php
@@ -3,96 +3,105 @@
 
 <head>
   <title>portfolio</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php wp_head(); ?>
 </head>
 
 <body>
-  <div class="container">
-    <header class="main_header">
-      <nav class="navbar">
-        <div class="header_logo">
-          <a href="<?php echo home_url(); ?>"><img src="<?php bloginfo('template_directory'); ?>/img/logo.jpg" alt="logo"></a>
-        </div>
-        <ul>
-          <li class="dropdown"> 概要
-            <ul class="dropdown_menu">
-              <li><?php $args = array('menu' => '概要', 'menu_class' => 'menu', 'container' => false,);
-                  wp_nav_menu($args); ?></li>
+  <div class="wrapper">
+    <header>
+      <div class="main_header">
+        <nav class="navbar">
+          <div class="header_logo">
+            <a href="<?php echo home_url(); ?>"><img src="<?php bloginfo('template_directory'); ?>/img/logo1.jpg" alt="logo"></a>
+          </div>
+          <div class="header_menu">
+            <ul>
+              <li class="dropdown"> 概要
+                <ul class="dropdown_menu">
+                  <li><?php $args = array('menu' => '概要', 'menu_class' => 'menu', 'container' => false,);
+                      wp_nav_menu($args); ?></li>
+                </ul>
+              </li>
             </ul>
-          </li>
-        </ul>
-        <ul>
-          <li class="dropdown">詳しく
-            <ul class="dropdown_menu">
-              <li><?php $args = array('menu' => '趣味', 'menu_class' => 'menu', 'container' => false,);
-                  wp_nav_menu($args); ?></li>
+            <ul>
+              <li class="dropdown">詳しく
+                <ul class="dropdown_menu">
+                  <li><?php $args = array('menu' => '詳しく', 'menu_class' => 'menu', 'container' => false,);
+                      wp_nav_menu($args); ?></li>
+                </ul>
             </ul>
-        </ul>
-        </li>
-        <ul>
-          <li class="dropdown">アクセス
-            <ul class="dropdown_menu">
-              <li><?php $args = array('menu' => '趣味', 'menu_class' => 'menu', 'container' => false,);
-                  wp_nav_menu($args); ?></li>
+            </li>
+            <ul>
+              <li class="dropdown">アクセス
+                <ul class="dropdown_menu">
+                  <li><?php $args = array('menu' => 'アクセス', 'menu_class' => 'menu', 'container' => false,);
+                      wp_nav_menu($args); ?></li>
+                </ul>
+              </li>
             </ul>
-          </li>
-        </ul>
-        <ul>
-          <li class="dropdown">趣味
-            <ul class="dropdown_menu">
-              <li><?php $args = array('menu' => '趣味', 'menu_class' => 'menu', 'container' => false,);
-                  wp_nav_menu($args); ?></li>
+            <ul>
+              <li class="dropdown">趣味
+                <ul class="dropdown_menu">
+                  <li><?php $args = array('menu' => '趣味', 'menu_class' => 'menu', 'container' => false,);
+                      wp_nav_menu($args); ?></li>
+                </ul>
+              </li>
             </ul>
-          </li>
-        </ul>
-      </nav>
-    </header>
-    <main>
-      <div class="main_slider">
-        <ul class="slider">
-          <img src="<?php bloginfo('template_directory'); ?>/img/slide-3.jpg" alt="image3">
-          <img src="<?php bloginfo('template_directory'); ?>/img/slide-5.jpg" alt="image5">
-          <img src="<?php bloginfo('template_directory'); ?>/img/slide-6.jpg" alt="image6">
-          <img src="<?php bloginfo('template_directory'); ?>/img/slide-7.jpg" alt="image7">
-          <img src="<?php bloginfo('template_directory'); ?>/img/slide-8.jpg" alt="image8">
-        </ul>
+          </div>
+        </nav>
       </div>
-    </main>
-    <div class="news">
-      <div class="newe_title">新着情報</div>
-     <?php $wpwp_posts = new WP_Query(array('posts_per_page' => 3));
-      ?>
-      <?php if ($wpwp_posts->have_posts()) :
-        while ($wpwp_posts->have_posts()) : $wpwp_posts->the_post();
-      ?>
-          <div class="">
-            <dl class="">
-              <dt class="">
+    </header>
+    <div class="main_wrapper">
+      <main>
+
+        <div class="main_slider">
+          <ul class="slider">
+            <img src="<?php bloginfo('template_directory'); ?>/img/slide-3.jpg" alt="image3">
+            <img src="<?php bloginfo('template_directory'); ?>/img/slide-5.jpg" alt="image5">
+            <img src="<?php bloginfo('template_directory'); ?>/img/slide-6.jpg" alt="image6">
+            <img src="<?php bloginfo('template_directory'); ?>/img/slide-7.jpg" alt="image7">
+            <img src="<?php bloginfo('template_directory'); ?>/img/slide-8.jpg" alt="image8">
+          </ul>
+        </div>
+      </main>
+      <div class="news">
+        <div class="news_title">新着情報</div>
+        <?php $wpwp_posts = new WP_Query(array('posts_per_page' => 5));
+        ?>
+        <?php if ($wpwp_posts->have_posts()) :
+          while ($wpwp_posts->have_posts()) : $wpwp_posts->the_post();
+        ?>
+            <dl class="news_container">
+              <dt class="news_container-">
+              <dd class="">
                 <?php the_time('Y年m月d日（D）'); ?>
                 <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+              </dd>
               </dt>
             </dl>
-          </div>
-        <?php endwhile; ?>
-        <?php wp_reset_postdata(); ?>
-      <?php else : ?>
-        記事はありません。
-      <?php endif; ?>
-      <div class="news_archive">
-        <a href="<?php echo get_post_type_archive_link('example'); ?>">＜＜記事一覧＞＞</a>
+          <?php endwhile; ?>
+          <?php wp_reset_postdata(); ?>
+        <?php else : ?>
+          記事はありません。
+        <?php endif; ?>
+        <div class="news_archive">
+          <a href="<?php echo get_post_type_archive_link('example'); ?>">＜記事一覧＞</a>
+        </div>
       </div>
-    </div>
 
-  <footer>
-    <div class="main_footer">
+      <footer>
+        <div class="main_footer">
 
-      <ul class="footer-icon">
-        <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
-        <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
-        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-      </ul>
+          <ul class="footer_icon">
+            <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+            <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+          </ul>
+        </div>
+
+      </footer>
     </div>
-  </footer>
   </div>
   <?php wp_footer(); ?>
 </body>
