d035d41f49e80b2d033c0a3670be8e1b573516d0
kazuma kobari <sprnrml_crr@icloud.com>
2020-10-27 18:09:05 +0900

    2020/10/27

--- archive.php
+++ archive.php
@@ -3,7 +3,7 @@
   <div class="category">
     <p>＜ カテゴリー 一覧 ＞</p>
     <ul>
-      <?php wp_list_categories('title_li=&orderby=ID'); ?>
+      <?php wp_list_categories('title_li=&orderby=ID&child_of=12'); ?>
     </ul>
   </div>
   <div class="news">
@@ -11,26 +11,33 @@
     <?php
     $terms = get_terms('category', array(
       'parent' => '',
-      'hide_empty' => true,
-      'orderby' => 'term_order',
     ));
     foreach ($terms as $term)
     ?>
-    <?php
-  if (have_posts()) :
+   <?php
+    if (have_posts()) :
     while (have_posts()) : the_post();
     ?>
+        <?php
+        $category = get_the_category();
+        $cat_id   = $category[0]->cat_ID;
+        $cat_name = $category[0]->cat_name;
+        $cat_slug = $category[0]->category_nicename;
+        ?>
         <div class="news_container">
           <?php the_time('Y.m.d.'); ?>
           <span class="news_category"><?php echo $cat_name; ?></span>
           <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
         </div>
+
       <?php endwhile; ?>
-      <!-- <?php if (function_exists("the_pagination")) the_pagination(); ?> -->
+
     <?php else : ?>
       記事はありません。
     <?php endif; ?>
+    <?php if (function_exists("the_pagination")) the_pagination(); ?>
     <div class="news_archive">
+      <a href="<?php echo get_category_link(12); ?>">＜List of articles＞</a>
       <a href="<?php echo home_url(); ?>">＜home＞</a>
     </div>
   </div>
